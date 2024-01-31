44eab044e04a03b74fb595b12a95f8ab6983941b
wasquel.valentin <wasquel.valentin@gmail.com>
2024-01-31 18:20:08 +0100

    add basic slash

--- src/main/java/k6v/BasicCommands.java
+++ src/main/java/k6v/BasicCommands.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.Permission;
@@ -30,51 +31,80 @@ public class BasicCommands {
     }
 
 
-    public static void ping(MessageReceivedEvent event)
+    public static void ping(MessageChannel channel)
     {
-        MessageChannel channel = event.getChannel();
-        JDA k6v = event.getJDA();
+        JDA k6v = channel.getJDA();
         channel.sendMessage("pong !").queue();
         long pingRest = k6v.getRestPing().complete();
         long pingHeart = k6v.getGatewayPing();
         channel.sendMessageFormat("Rest action ping %d ms \n HeartBeat ping %d ms", pingRest, pingHeart).queue();
     }
 
-    public static void updateSlash(MessageReceivedEvent event)
+    public static void ping(SlashCommandInteractionEvent event)
     {
-        MessageChannel channel = event.getChannel();
-        event.getJDA().updateCommands().addCommands(getGlobalCommands())
+        JDA k6v = event.getJDA();
+        event.reply("pong !").queue();
+        long pingRest = k6v.getRestPing().complete();
+        long pingHeart = k6v.getGatewayPing();
+        event.getHook().editOriginalFormat("pong !\nRest action ping %d ms \n HeartBeat ping %d ms", pingRest, pingHeart).queue();
+    }
+
+    public static void updateSlash(MessageChannel channel)
+    {
+        channel.getJDA().updateCommands().addCommands(getGlobalCommands())
             .queue(
                 (v) -> channel.sendMessage("Les commands slash ont été mis à jour").queue(), 
                 (v) -> channel.sendMessage("il y a eu une erreur dans la mise a jour des commands").queue()
             );
     }
 
-    public static void whoAmI(MessageReceivedEvent event)
+    public static void updateSlash(SlashCommandInteractionEvent event)
     {
-        event.getChannel().sendMessage(
-            "you are " + event.getAuthor().getAsMention() + 
-            ", your id is " + event.getAuthor().getId() + 
-            "\nthe server is " + event.getGuild().getName() + 
-            " (" + event.getGuild().getId() + ")"
+        event.deferReply(true);
+        event.getJDA().updateCommands().addCommands(getGlobalCommands())
+            .queue(
+                (v) -> event.getHook().sendMessage("Les commands slash ont été mis à jour").queue(), 
+                (v) -> event.getHook().sendMessage("il y a eu une erreur dans la mise a jour des commands").queue()
+            );
+    }
+
+    public static void whoAmI(MessageChannel channel, User user, Guild guild)
+    {
+        channel.sendMessage(
+            "you are " + user.getAsMention() + 
+            ", your id is " + user.getId() + 
+            "\nthe server is " + guild.getName() + 
+            " (" + guild.getId() + ")"
         ).queue();
     }
 
-    public static void sendlink(MessageReceivedEvent event)
+    public static void whoAmI(SlashCommandInteractionEvent event, User user, Guild guild)
     {
-        MessageChannel channel = event.getChannel();
-        if (! BasicCommands.isUserOp(event.getAuthor())) 
-        {
-            channel.sendMessage("Tu n'est pas authorisé à utilisé cette commande.").queue();
-            return;
-        }
-        event.getChannel().sendMessage(event.getJDA().getInviteUrl(Permission.getPermissions(402182634816l))).queue();
+        event.reply(
+            "you are " + user.getAsMention() + 
+            ", your id is " + user.getId() + 
+            "\nthe server is " + guild.getName() + 
+            " (" + guild.getId() + ")"
+        ).queue();
+    }
+
+    public static void sendlink(MessageChannel channel)
+    {
+        channel.sendMessage(channel.getJDA().getInviteUrl(Permission.getPermissions(402182634816l))).queue();
+    }
+
+    public static void sendlink(SlashCommandInteractionEvent event)
+    {
+        event.reply(event.getJDA().getInviteUrl(Permission.getPermissions(402182634816l))).queue();
     }
 
     protected static Collection<CommandData> getGlobalCommands()
     {
         Collection<CommandData> result = new ArrayList<CommandData>();
         result.add(Commands.slash("joinvoice", "join the vocal you are in"));
+        result.add(Commands.slash("ping", "get ping information"));
+        result.add(Commands.slash("link", "get the link to invite the bot (need to be op)"));
+        result.add(Commands.slash("whoAmI", "get information about your Id and the guild Id"));
         return result;
     }
 }
--- src/main/java/k6v/CommandHandler.java
+++ src/main/java/k6v/CommandHandler.java
@@ -1,6 +1,8 @@
 package k6v;
 
-import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,26 +11,72 @@ public class CommandHandler extends ListenerAdapter
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if (event.getAuthor().isBot()) return;
+        User user = event.getAuthor();
+        if (user.isBot()) return;
         // We don't want to respond to other bot accounts, including ourself
         Message message = event.getMessage();
         String content = message.getContentRaw(); 
+        MessageChannel channel = event.getChannel();
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
         if (content.equals("!ping"))
         {
-            BasicCommands.ping(event);
+            BasicCommands.ping(channel);
         }
         if (content.equals("!updateSlash"))
         {
-            BasicCommands.updateSlash(event);
+            if (! BasicCommands.isUserOp(user)) 
+            {
+                channel.sendMessage("Tu n'est pas authorisé à utilisé cette commande.").queue();
+                return;
+            }
+            BasicCommands.updateSlash(channel);
         }
         if (content.equals("!whoAmI"))
         {
-            BasicCommands.whoAmI(event);
+            BasicCommands.whoAmI(channel, user, event.getGuild());
         }
         if (content.equals("!link"))
         {
+            if (! BasicCommands.isUserOp(user)) 
+            {
+                channel.sendMessage("Tu n'est pas authorisé à utilisé cette commande.").queue();
+                return;
+            }
+            BasicCommands.sendlink(channel);
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        User user = event.getUser();
+        if (user.isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        String name = event.getName();
+        if (name.equals("ping"))
+        {
+            BasicCommands.ping(event);
+        }
+        if (name.equals("updateSlash"))
+        {
+            if (! BasicCommands.isUserOp(user)) 
+            {
+                event.reply("Tu n'est pas authorisé à utilisé cette commande.").queue();
+                return;
+            }
+            BasicCommands.updateSlash(event);
+        }
+        if (name.equals("whoAmI"))
+        {
+            BasicCommands.whoAmI(event, user, event.getGuild());
+        }
+        if (name.equals("link"))
+        {
+            if (! BasicCommands.isUserOp(user)) 
+            {
+                event.reply("Tu n'est pas authorisé à utilisé cette commande.").queue();
+                return;
+            }
             BasicCommands.sendlink(event);
         }
     }
